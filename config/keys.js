3798f17e7e15b94e31598e033217dca3a740ba1f
hemant-crv <hemantchoudhary269@gmail.com>
2018-10-08 04:53:24 +0000

    first

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {      
+        clientID: '89378607450-0fa9fpji4q0r40krb5pcm3rjmckqcsg6.apps.googleusercontent.com',
+        clientSecret: 'J6CLcwgIM17zjRf--PXspQ_z'
+    },
+    mongodb: {
+        dbURI: 'mongodb://user:user12@ds121163.mlab.com:21163/userdata'
+    },
+    session: {
+        cookieKey: 'dgsdjtkirtjktrjtjuryt'
+    }
+};
